066fc42e5d76969f7bbf1c604006a16df2eb7443
Paul LeTourneau <pletourneau62@gmail.com>
2025-06-11 19:19:37 -0700

    well, ill be unemployed forever

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ createApp({
             <p><strong>Hack:</strong> To get an events module to display only 'in-person' events, start by selecting any other options for the event (location, audience, event type, etc) and also select 'online events'. Copy and paste the URL in the module, but make one small addition. At the end of the URL should be 'locations=BC_VIRTUAL'. This is where you add the bang operator so it will now read 'locations=!BC_VIRTUAL' (now it says 'NOT BC_VIRTUAL'). Your event feed shows only in person events.</p>
             <p><strong>Huzzah!</strong></p>
           `,
-          previewImages: ["assets/images/BC.jpg"],
+          previewImages: ["assets/images/!cool.webp"],
           currentImageIndex: 0,
           open: false,
         },
